928b893e87e4960ce7ab0df60fbe9a3ec3339948
abdullah <abdullah>
2023-02-13 00:10:14 +0530

    initial creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AudoPlayer';
+  @ViewChild('myplayer') myplayer: ElementRef<HTMLAudioElement>;
+  get audioPlayer(){
+    return this.myplayer.nativeElement;
+  }
+  onSelect(event) {
+    this.myplayer.nativeElement.src = URL.createObjectURL(event.target.files[0])
+  }
+
+  forward(){
+    this.audioPlayer.currentTime += 10;
+  }
+  backward(){
+    this.audioPlayer.currentTime -= 10;
+  }
 }
